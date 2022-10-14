426c300e1db7449e19a0d741491817a8623673c7
kristinkaklyap <kristinka.klyap@gmail.com>
2022-10-14 14:20:21 +0200

    carousel

---
+++ src/components/UI/Carousel.js
@@ -0,0 +1,88 @@
+import classes from './Carousel.module.css'
+import {useEffect, useState} from "react";
+
+const Carousel = (props) => {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slides = props.slides;
+    const settings = props.settings
+
+    console.log(settings)
+
+    const dotClickHandler = (e) => {
+        const currentDot = e.target;
+        const currentDotIndex = currentDot.getAttribute('id')?.slice(-1);
+
+        setCurrentSlide(currentDotIndex)
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const slideIndex = +currentSlide + 1
+            setCurrentSlide(slideIndex)
+        }, 7000)
+
+        return () => {
+            if (interval) {
+                clearInterval(interval)
+            }
+        }
+
+    })
+
+    useEffect(() => {
+        const infinite = () => {
+            if (Object.keys(slides).length === (currentSlide)) {
+                setCurrentSlide(0)
+            }
+        }
+        infinite()
+    })
+
+
+    return (
+        <div className={classes.carousel_container}>
+            {Object.entries(slides).map(slide => {
+                const slideIndex = slide[0]
+                const slideImg = slide[1].image || null;
+                const slideTitle = slide[1].title || null;
+                const slideText = slide[1].text || null;
+
+
+                const slideClasses = currentSlide == slideIndex ? `${classes.carousel_container_item} ${classes['active']}` : classes.carousel_container_item
+                return (
+                    <div key={`slide-${slideIndex}`} id={`slide-${slideIndex}`} className={slideClasses}
+                         style={{transform: `translateX(-${currentSlide * 100}%)`}}
+                    >
+                        {slideImg && settings.image &&
+                            <div className={classes['carousel_container_item--image']}>
+                                <img src={slideImg}/>
+                            </div>
+                        }
+                        {(settings.title || settings.text) &&
+                            <div className={classes['carousel_container_item--content']}>
+                                {slideTitle && settings.title &&
+                                    <div className={classes['carousel_container_item--title']}>{slideTitle}</div>}
+                                {slideText && settings.text && <div>{slideText}</div>}
+                            </div>
+                        }
+
+
+                    </div>)
+            })}
+            {settings.dots &&
+                <div className={classes['carousel_container--dots']}>
+                    {Object.entries(slides).map(item => {
+                        const dotIndex = item[0];
+                        const dotClasses = currentSlide == dotIndex ? `${classes['dot']} dot-${dotIndex} ${classes['active']}` : `${classes['dot']} dot-${dotIndex}`
+
+                        return (<span onClick={dotClickHandler} id={`dot-${dotIndex}`} key={`dot-${dotIndex}`}
+                                      className={dotClasses}></span>)
+                    })}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Carousel
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,43 +6,101 @@ import ProductsList from "../components/Products/ProductsList";
 import TextField from "../components/UI/TextField";
 
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 import Button from "../components/UI/Button";
 import Container from "../components/UI/Container";
+import Carousel from "../components/UI/Carousel";
+
+import slider1 from "../assets/slider-1.png"
+import slider2 from "../assets/slider-2.jpg"
 
 const Homepage = () => {
-  const navigate = useNavigate();
-  return (
-    <>
-      <Banner bgImage={bgImage} imgAlt={"Macbook Pro and Magic Mouse"}>
-        <h1>
-          Zadbajmy <span className={bannerStyles.highlighted}>wspólnie</span>
-          <span className={bannerStyles.moved}>o wygodę Twojej pracy...</span>
-        </h1>
-      </Banner>
-      <section>
-        <SectionTitle headingLevel={3} title={"Podkładki na biurko"} />
-        <ProductsList />
-        <Button listofclasses={['button--primary','centered','margin-top-20']} text={"wszystkie produkty"} onClick={ () => navigate("/products")} />
-      </section>
-      <section>
-        <Container>
-          <SectionTitle headingLevel={3} title={"Inspiracje"} />
-          <TextField listofclasses={['centered-text', 'limited']}>
-            <p>Dobrze wiemy, że podstawą efektywnej pracy jest zarówno czysty umysł
-              jak i uporządkowane biurko. Nie pozwól żeby „artystyczny nieład” panujący
-              na biurku rozpraszał Cię  podczas tworzenia cudów....
-            </p>
-          </TextField>
-        </Container>
+    const navigate = useNavigate();
 
-      </section>
-      <section>
+    const carouselSlides = {
+        0: {
+            image: slider1,
+            title: 'Oświetlenie',
+            text: 'Zadbaj o dobre oświetlenie miejsca pracy...\n' +
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \n' +
+                'sed do eiusmod tempor incididunt ut labore et dolore \n' +
+                'magna aliqua. Ut enim ad minim veniam, quis nostrud \n' +
+                'exercitation ullamco laboris nisi ut aliquip ex ea \n' +
+                'commodo consequat. Duis aute irure dolor in \n' +
+                'reprehenderit in voluptate velit esse cillum dolore eu \n' +
+                'fugiat nulla pariatur. Excepteur sint occaecat cupidatat\n' +
+                ' non proident, sunt in culpa qui officia deserunt mollit \n' +
+                'anim id est laborum.'
+        }, 1: {
+            image: slider2,
+            title: 'Porządek',
+            text: 'Zadbaj o porządek miejsca pracy...\n' +
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \n' +
+                'sed do eiusmod tempor incididunt ut labore et dolore \n' +
+                'magna aliqua. Ut enim ad minim veniam, quis nostrud \n' +
+                'exercitation ullamco laboris nisi ut aliquip ex ea \n' +
+                'commodo consequat. Duis aute irure dolor in \n' +
+                'reprehenderit in voluptate velit esse cillum dolore eu \n' +
+                'fugiat nulla pariatur. Excepteur sint occaecat cupidatat\n' +
+                ' non proident, sunt in culpa qui officia deserunt mollit \n' +
+                'anim id est laborum.'
+        }, 2: {
+            image: slider1,
+            title: 'Kolejny slide',
+            text: 'Zadbaj o porządek miejsca pracy...\n' +
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \n' +
+                'sed do eiusmod tempor incididunt ut labore et dolore \n' +
+                'magna aliqua. Ut enim ad minim veniam, quis nostrud \n' +
+                'exercitation ullamco laboris nisi ut aliquip ex ea \n' +
+                'commodo consequat. Duis aute irure dolor in \n' +
+                'reprehenderit in voluptate velit esse cillum dolore eu \n' +
+                'fugiat nulla pariatur. Excepteur sint occaecat cupidatat\n' +
+                ' non proident, sunt in culpa qui officia deserunt mollit \n' +
+                'anim id est laborum.'
+        },
+    }
+    const carouselSettings = {
+        dots: true,
+        image: true,
+        title: true,
+        text: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    }
+    return (
+        <>
+            <Banner bgImage={bgImage} imgAlt={"Macbook Pro and Magic Mouse"}>
+                <h1>
+                    Zadbajmy <span className={bannerStyles.highlighted}>wspólnie</span>
+                    <span className={bannerStyles.moved}>o wygodę Twojej pracy...</span>
+                </h1>
+            </Banner>
+            <section>
+                <SectionTitle headingLevel={3} title={"Podkładki na biurko"}/>
+                <ProductsList/>
+                <Button listofclasses={['button--primary', 'centered', 'margin-top-20']} text={"wszystkie produkty"}
+                        onClick={() => navigate("/products")}/>
+            </section>
+            <section>
+                <Container>
+                    <SectionTitle headingLevel={3} title={"Inspiracje"}/>
+                    <TextField listofclasses={['centered-text', 'limited']}>
+                        <p>Dobrze wiemy, że podstawą efektywnej pracy jest zarówno czysty umysł
+                            jak i uporządkowane biurko. Nie pozwól żeby „artystyczny nieład” panujący
+                            na biurku rozpraszał Cię podczas tworzenia cudów....
+                        </p>
+                    </TextField>
+                </Container>
 
-      </section>
+            </section>
+            <section>
+                <Carousel slides={carouselSlides} settings={carouselSettings}></Carousel>
+            </section>
 
-    </>
-  );
+        </>
+    );
 };
 export default Homepage;
